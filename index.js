a4adfa39e6693fec5f7641655ab20eeeb838ed40
NoobZombieTR <38327391+NoobZombieTR@users.noreply.github.com>
2018-04-26 17:49:18 +0300

    c

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
 	
   console.log(`${bot.user.username} adlı bot ${bot.guilds.size} sunucusunda online!`);
-  bot.user.setActivity(`&yardim - ${bot.guilds.size} sunucuda online!`, {url: "https://www.twitch.tv/directory/game/Minecraft"});
+  bot.user.setActivity(`loyardım - ${bot.guilds.size} sunucuda eğleniyor!`, {url: "https://www.twitch.tv/directory/game/Minecraft"});
 	
 
 });
@@ -23,17 +23,46 @@ bot.on("message", async message => {
 var msg = message.content.toUpperCase();
   var sender = message.author;
 
-	
+	if(cmd === `${prefix}yardım`){
+	let helpEmbed1 = new Discord.RichEmbed()
+    .setTitle("Lokman Yardım Sayfası")
+    .setColor(botconfig.pembe)
+    .addField("Eğlence Komutları", "&ban <@kullanıcı adı> <sebep>: Kullanıcıyı sunucudan yasaklar.")
+    .addField("Atma", "&kick <@kullanıcı adı> <sebep>: Kullanıcıyı sunucudan atar.")
+    .addField("Mesajları Silme", "&del: Tüm mesajları siler.")
+    message.channel.send(helpEmbed1);
+	}
 	
 
-	
-	if(cmd === `${prefix}limonbebek`){
+	 async run(message) {
+    let toMeme = ['https://i.redd.it/0ilh488xbudz.png',
+      'https://cdn.discordapp.com/attachments/310611569794875404/353539349742092289/image.jpg',
+      'http://weknowmemes.com/wp-content/uploads/2012/02/the-internet-is-a-series-of-tubes-and-theyre-full-of-cats.jpg',
+    ]
+}
+	toMeme = toMeme[Math.floor(Math.random() * toMeme.length)]
+    message.channel.send(toMeme)
+	if(cmd === `${prefix}gif`){
 	message.channel.send("", {
-    file: "http://www.hurriyetaile.com/userfiles/images/animation-1-3.gif" // Or replace with FileOptions object
-});
-		}
-
-
+    file: "http://www.hurriyetaile.com/userfiles/images/animation-1-3.gif"
+	});
+	}
+if(cmd === `${prefix}gm`){
+async run(message, args) {
+		const annChan = this.client.channels.get('282977399761666059')
+		const annRole = message.guild.roles.get('338046233765478401')
+		annRole.setMentionable(true)
+    const embed = new RichEmbed()
+    .setAuthor(message.author.username, message.author.avatarURL)
+		.setDescription(args.toAnn)
+		.setColor('0xFF0000')
+    .setTimestamp();
+    annChan.send('<@&338046233765478401>', { embed }).then((msg) => {
+        message.reply('Announcement sent!')
+		})
+		annRole.setMentionable(false)
+  }
+}
 });
 
 bot.login(process.env.BOT_TOKEN);
